ade9bbcfe24dc75c64d24f3cc1009361080c2418
Sotrosca <facuserna93@gmail.com>
2019-03-16 16:01:01 -0300

    Actualizado

--- paperio_simulacion.py
+++ paperio_simulacion.py
@@ -1,12 +1,16 @@
+# -*- coding: utf-8 -*-
 import numpy as np
 
 
 class tablero():
-	def __init__(self, base, altura, tamañoCasaInicial, cantidadDeJugadores, posicionJugadores): ##posicionJugadores es un vector con las coordenadas x e y de cada jugadora al iniciar
-																									#tamañoCsaInicial es un numero impar que da el caudrado en el que inician 
+	#tamañoCsaInicial es un numero impar que da el caudrado en el que inician 
+	#posicionJugadores es un vector con las coordenadas x e y de cada jugadora al iniciar
+
+	def __init__(self, base, altura, tamanoCasaInicial, cantidadDeJugadores, posicionJugadores): 
+																									
 		self.base = base
 		self.altura = altura
-		self.tamañoCasaInicial = tamañoCasaInicial
+		self.tamanoCasaInicial = tamanoCasaInicial
 		self.cantidadDeJugadores = cantidadDeJugadores
 		self.posicionJugadores = posicionJugadores
 
@@ -17,7 +21,7 @@ class jugadore():
 		self.playerID = playerID
 		self.direccionInicial =  np.random.randint(1, 5)
 		self.posicionInicial = tablero.posicionJugadores[playerID - 1]
-		self.tamañoCasaInicial = tablero.tamañoCasaInicial
+		self.tamanoCasaInicial = tablero.tamanoCasaInicial
 		self.matrizTerritorio = np.zeros((tablero.altura + 2, tablero.base + 2))
 		
 		vectorAux1 = np.ones((tablero.base, 1))
@@ -25,8 +29,8 @@ class jugadore():
 		matrizAux = np.zeros((tablero.base, tablero.altura))
 		self.matrizDeCamino = np.concatenate((vectorAux2, np.concatenate((vectorAux1, matrizAux, vectorAux1), axis=1).T, vectorAux2), axis=1)
 
-		for i in list(range(self.posicionInicial[0] - int((self.tamañoCasaInicial - 1) / 2), self.posicionInicial[0] + int((self.tamañoCasaInicial + 1) / 2))):
-			for j in list(range(self.posicionInicial[1] - int((self.tamañoCasaInicial - 1) / 2), self.posicionInicial[1] + int((self.tamañoCasaInicial + 1) / 2))):
+		for i in list(range(self.posicionInicial[0] - int((self.tamanoCasaInicial - 1) / 2), self.posicionInicial[0] + int((self.tamanoCasaInicial + 1) / 2))):
+			for j in list(range(self.posicionInicial[1] - int((self.tamanoCasaInicial - 1) / 2), self.posicionInicial[1] + int((self.tamanoCasaInicial + 1) / 2))):
 				self.matrizTerritorio[i, j] = 1
 
 
@@ -35,8 +39,8 @@ class jugadore():
 tablero1 = tablero(10, 10, 3, 2, [(3,3),(7,6)])
 Facu = jugadore('Facu', 1, tablero1)
 
-print(Facu.matrizDeCamino)
+#print(Facu.matrizDeCamino)
 #Sol = jugadore('Sol', 2, tablero1)
-#print(Facu.matrizTerritorio)
-#print(Sol.matrizTerritorio)
+print(Facu.matrizTerritorio)
+#print(Sol.matrizDeCamino)
 
